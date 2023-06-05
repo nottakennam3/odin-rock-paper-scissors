bb7a80d58111a3ababdc7adf0d6640f273cafaa7
Nghia Pham <jemapppellenghia@gmail.com>
2023-06-05 17:54:56 +0700

    Add buttons for player selection and game reset Display current score and annouce winner

--- game-script.js
+++ game-script.js
@@ -7,51 +7,50 @@ function getComputerChoice() {
     else return "Scissors";
 }
 
-function playRound(computerChoice) {
-    let playerSelection = prompt("Input your choice:").toLowerCase();
-    let firstChar = playerSelection[0].toUpperCase(); 
-    let restPart = playerSelection.slice(1);
-    let playerChoice = firstChar + restPart;
-    let result;
+function playRound(playerChoice) { 
+    let computerChoice = getComputerChoice();   
     if (playerChoice === computerChoice)
-        result = 0;
+        roundResult.textContent = "Draw!";
     else if (playerChoice === "Rock" && computerChoice === "Scissors" ||
              playerChoice === "Paper" && computerChoice === "Rock" ||
              playerChoice === "Scissors" && computerChoice === "Paper")
-        result = 1;
-    else result = 2;
-    return result;
+        {
+            score[0]++;
+            roundResult.textContent = `You win! ${playerChoice} beats ${computerChoice}!`;
+        }
+    else 
+        {
+            score[1]++;
+            roundResult.textContent = `You lose! ${computerChoice} beats ${playerChoice}`;
+        }
+    displayScore();   
 }
 
-function game() {
-    console.log("Game Start!");
-    let playerScore = 0;
-    let computerScore = playerScore;
-    let roundResult;
-    for (let i = 1; i <= 5; i++)
-    {
-        console.log(`Round ${i}`);
-        console.log(`Current Score: Player ${playerScore} : ${computerScore} Computer`)
-        roundResult = playRound(getComputerChoice());
-        if (roundResult == 0)
-            console.log("Draw!");
-        else if (roundResult == 1)
-        {
-            console.log("You win!");
-            playerScore++;
-        }
-        else 
-        {
-            console.log("You lose!");
-            computerScore++;
-        }
-    }
-    console.log(`Final Score: Player ${playerScore} : ${computerScore} Computer`);
-    if (playerScore == computerScore)
-        console.log("Game Draw!");
-    else if (playerScore > computerScore)
-        console.log("Congratulations! You win!");
-    else console.log("You lose! Better luck next time...");
+function displayScore() {
+    currentScore.textContent = `Current Score: Player ${score[0]}:${score[1]} Computer`;
+    if (score[0] == 5)
+        currentScore.textContent = `Final Score: Player ${score[0]}:${score[1]} Computer. You win!`;
+    else if (score[1] == 5)
+    currentScore.textContent = `Final Score: Player ${score[0]}:${score[1]} Computer. You lose!`;
 }
 
-game();
+let score = [0, 0];
+const rock = document.querySelector('#btn-rock');
+const paper = document.querySelector('#btn-paper');
+const scissors = document.querySelector('#btn-scissors');
+const reset = document.querySelector('#btn-reset');
+const roundResult = document.querySelector('.result');
+const currentScore = document.querySelector('.current-score');
+const btn = document.querySelector('#btn');
+
+currentScore.textContent = `Current Score: Player ${score[0]}:${score[1]} Computer`;
+
+rock.addEventListener("click", function (){playRound('Rock');});
+paper.addEventListener("click", function (){playRound('Paper');});
+scissors.addEventListener("click", function (){playRound('Scissors');});
+reset.addEventListener('click', () => {
+    score = [0, 0];
+    displayScore();
+    roundResult.textContent = "Click any above buttons to start the game";
+});
+
